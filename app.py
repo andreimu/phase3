ce6b98343069696c89f7fe197400d32a1a672cec
andreimu <andrei.muzikant@gmail.com>
2025-05-14 21:34:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+"""This flask app prints Hello from GitHub Actions!"""
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from GitHub Actions + Docker!"
+    """Function printing Hello from GitHub Actions!"""
+    return "Hello from GitHub Actions!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
